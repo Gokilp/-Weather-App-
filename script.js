4e01854be5b3cbc610421eeed9ed8ed75907d01a
gokilp <gokilp25@gmail.com>
2024-05-06 17:22:59 +0530

    fix bug

--- script.js
+++ script.js
@@ -3,26 +3,43 @@ const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weathericon = document.querySelector(".Weather-icon");
 
 async function gerateWeather(city) {
-    const response = await fetch(url + city + "&appid=" + apikey);
-    const data = await response.json();
-    console.log(data);
-  
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity; 
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+  const response = await fetch(url + city + "&appid=" + apikey);
+  const data = await response.json();
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity;
+  document.querySelector(".wind").innerHTML = data.wind.speed;
+  if(data.weather[0].icon == "Clouds") 
+    {
+      weathericon.src = "images/cloud.png";
+    } else if(data.weather[0].icon == "Clear") {
+      weathericon.src = "images/clear.png";
+  } else if (data.weather[0].icon == "Rain") {
+    weathericon.src = "images/rain.png";
+     }else if(data.weather[0].icon == "Drizzle") {
+      weathericon.src = "images/drizzle.png";
+  }
+  else if (data.weather[0].icon == "Mist") {
+    weathericon.src = "images/mist.png";
+}
 }
 
-searchBtn.addEventListener("click", () => {
-    const city = searchBox.value.trim(); 
-    if (city) {
-        gerateWeather(city); 
-    } else {
-        alert("Please enter a city name.");
-    }
-});   
+const handleCitySearch = () => {
+  const city = searchBox.value.trim();
+  if (city) {
+    gerateWeather(city);
+  } 
+};
 
-console.log(city);
+searchBtn.addEventListener("click", handleCitySearch);
 
+searchBox.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    handleCitySearch();
+  }
+});
